6db71ef167e21dc1dc9941ea73dbd8c7f899806f
tymaut <yvzkzk@gmail.com>
2014-09-30 22:02:57 +0300

    working version

--- words/python.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import MySQLdb
-db = MySQLdb.connect(host="localhost",user="wordFindUser",passwd="password",db="WordFind")
-cur = db.cursor()
-file = open('words/words6.txt')
-print file.read()
